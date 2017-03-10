44fd9a1d0cb57785071c1fba4b357ea8e5d9a120
tony <by.zhu@live.cn>
2017-03-10 11:39:54 +0800

    dev: enum utils for transform

---
+++ src/main/java/me/tony/base/util/EnumUtils.java
@@ -0,0 +1,22 @@
+package me.tony.base.util;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by tony on 2017/3/9.
+ */
+public class EnumUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnumUtils.class);
+
+    public static <ET extends Enum<ET>, ES extends Enum<ES>> ET transformSilence(ES source, Class<ET> targetEnumType) {
+        try {
+            return Enum.valueOf(targetEnumType, source.name());
+        } catch (Exception e) {
+            LOGGER.warn("enum transform exception", e);
+            return null;
+        }
+    }
+}
